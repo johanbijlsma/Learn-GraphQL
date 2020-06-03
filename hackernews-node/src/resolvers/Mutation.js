c45a10a36babe5716c2a2cd332712ecf3ad1fc99
Johan Bijlsma <johan.bijlsma24@gmail.com>
2020-06-03 08:12:32 +0200

    seems broken, commit to figure out whats wrong

--- hackernews-node/src/resolvers/Mutation.js
+++ hackernews-node/src/resolvers/Mutation.js
@@ -1,51 +1,81 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { APP_SECRET, getUserId } = require('../utils')
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET, getUserId } = require('../utils');
 
 async function signup(parent, args, context, info) {
+	// 1
 	const hashedPassword = await bcrypt.hash(args.password, 10);
+	// 2
 	const { password, ...user } = await context.prisma.createUser({
 		...args,
 		password: hashedPassword,
 	});
+
+	// 3
 	const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+	// 4
 	return {
 		token,
 		user,
 	};
 }
 
-async function login(parent, args, context, info){
-    const {password, ...user} = await context.prisma.user({email: args.email})
-    if(!user) {
-        throw new Error('No such user found')
-    }
+async function login(parent, args, context, info) {
+	// 1
+	const { password, ...user } = await context.prisma.user({
+		email: args.email,
+	});
+	if (!user) {
+		throw new Error('No such user found');
+	}
 
-    const valid = await bcrypt.compare(args.password, password)
-    if (!valid){
-        throw new Error('Invalid Password')
-    }
+	// 2
+	const valid = await bcrypt.compare(args.password, password);
+	if (!valid) {
+		throw new Error('Invalid password');
+	}
 
-    const token = jwt.sign({userId: user.id}, APP_SECRET)
+	const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
-    return {
-        token, user,
-    }
+	return {
+		token,
+		user,
+	};
+}
+
+async function vote(parent, args, context, info) {
+	// 1
+	const userId = getUserId(context);
+
+	// 2
+	const voteExists = await context.prisma.$exists.vote({
+		user: { id: userId },
+		link: { id: args.linkId },
+	});
+	if (voteExists) {
+		throw new Error(`Already voted for link: ${args.linkId}`);
+	}
+
+	// 3
+	return context.prisma.createVote({
+		user: { connect: { id: userId } },
+		link: { connect: { id: args.linkId } },
+	});
 }
 
 function post(parent, args, context, info) {
-  const userId = getUserId(context)
-  return context.prisma.createLink({
-    url: args.url,
-    description: args.description,
-    postedBy: { connect: { id: userId } },
-  })
+	const userId = getUserId(context);
+	return context.prisma.createLink({
+		url: args.url,
+		description: args.description,
+		postedBy: { connect: { id: userId } },
+	});
 }
-
 
 module.exports = {
-    signup,
-    login,
-    post
-}
+	signup,
+	login,
+	post,
+	vote,
+};
